11c4b2a8c386151b752e7cbd853bbd5078acdd2d
Ashraf Yusuf <ashrafowuda@yahoo.com>
2025-02-22 22:48:43 +0000

    Minor fixes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 from src.image_generation import generate_profanity_images
 
 def main():
@@ -9,9 +8,10 @@ def main():
     # Step 1: Generate images of profanities using different typefaces
     print("Starting image generation...")
     generate_profanity_images(profanities_file, fonts_folder, output_dir, include_obfuscations=True, multi_cases=True)
+    print("Image generation completed.")
     
     # Step 2: Pre-process images and prepare dataset
-    print("Preprocessing and preparing images")
+    print("Preprocessing and preparing images for training...")
     
     # Step 3: Train the CNN model on the processed data
     print("Training CNN model...")
